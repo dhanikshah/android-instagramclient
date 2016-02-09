aa4711a7641ca8124ae06f2d391f8654535ce7b5
dhanikshah <dhanikshah@users.noreply.github.com>
2016-02-09 01:33:16 -0800

    basic instagram client

---
+++ app/src/main/java/com/dhanshah/instagramclient/InstagramPhoto.java
@@ -0,0 +1,12 @@
+package com.dhanshah.instagramclient;
+
+/**
+ * Created by dhanikshah on 2/9/16.
+ */
+public class InstagramPhoto {
+    public String username;
+    public String caption;
+    public String imageUrl;
+    public int imageHeight;
+    public int likesCount;
+}
